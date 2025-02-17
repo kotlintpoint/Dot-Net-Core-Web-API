9dde359c9d71ca8054b82663e8c5d96ec59ae9a3
Kotlintpoint <ankit.sodha123@gmail.com>
2025-02-17 21:36:04 +0530

    Error Handling with ExceptionMiddleware

---
+++ GetriWebApi/Controllers/BuggyController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GetriWebApi.Controllers
+{
+    public class BuggyController : BaseApiController
+    {
+        [HttpGet("not-found")]
+        public ActionResult GetNotFound()
+        {
+            return NotFound();
+        }
+
+        [HttpGet("bad-request")]
+        public ActionResult GetBadRequest()
+        {
+            return BadRequest();
+        }
+
+        [HttpGet("server-error")]
+        public ActionResult GetServerError()
+        {
+            throw new Exception("This is server error.");
+        }
+
+        [HttpGet("unauthorized")]
+        public ActionResult Unauthorized()
+        {
+            return Unauthorized();
+        }
+    }
+}
--- GetriWebApi/Program.cs
+++ GetriWebApi/Program.cs
@@ -1,4 +1,5 @@
 using GetriWebApi.Extensions;
+using GetriWebApi.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +8,7 @@ builder.Services.AddApplicationServices(builder.Configuration);
 
 var app = builder.Build();
 
+app.UseMiddleware<ExceptionMiddleware>();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
